fe7edf4362dc31b13da5503617935342aaaac63b
Joanne Bogart <jrb@slac.stanford.edu>
2020-11-16 18:47:46 -0800

    add sql scripts to insert into truth summary tables

---
+++ sql/lensed_snetruth/lensed_sne_truth_summary_data.sql
@@ -0,0 +1,2 @@
+\copy lensed_sne_truth.truth_summary (id, host_galaxy, ra, dec, redshift, is_variable, is_pointsource, flux_u, flux_g, flux_r, flux_i, flux_z, flux_y, flux_u_noMW, flux_g_noMW, flux_r_noMW, flux_i_noMW, flux_z_noMW, flux_y_noMW) from '/global/cscratch1/sd/jrbogart/Run3.1i_truth/lensed_snetruth/truth_summary.csv' with (FORMAT 'csv', header)
+
